199aa8bcc2f5f6b14cc6182e80dc0239fe2e4db3
舒全刚 <645064175@qq.com>
2018-04-16 15:55:38 +0800

    跟新

---
+++ js/pages/roomDetail.js
@@ -0,0 +1,135 @@
+$(function(){
+    //筛选tab切换
+    $(".tabTable tr .td1").click(function(){
+        $(".tabconWrap .areaTab").show().siblings().hide();
+    });
+    $(".tabTable tr .td2").click(function(){
+        $(".tabconWrap .priceTab").show().siblings().hide();
+    });
+    $(".forTab").click(function(){
+        $(this).hide();
+    });
+    //房源图片轮播
+    new Swiper(".proImg", {
+        speed: 3000,
+        autoplay:true,
+        pagination: '.pagination',
+        paginationClickable: true,
+    });
+    //地图显示
+    var center = new qq.maps.LatLng(34.2919258755,108.9472800493);
+    var map = new qq.maps.Map(
+        document.getElementById("roomMap"),
+        {
+            center: center,
+            zoom: 13,
+            zoomControl: false,
+            mapTypeControlOptions: {
+                mapTypeIds: [
+                    qq.maps.MapTypeId.ROADMAP,
+                ]
+            }
+        }
+    );
+    var marker = new qq.maps.Marker({
+        position: center,
+        map: map
+    });
+})
+
+new Vue({
+    el: '#main',
+    data: {
+        pathUrl:conf.pathUrl,
+        id:'',
+        info:'',
+        commission:[],
+        tagsData:[],
+        roomtype:[],
+        salestatus:[], //销售状态
+        orientation:[],  //朝向
+        floorpostion:[], //楼层
+        decoratestyle:[], //装修
+        ownership:[], //权属
+        purpose:[],//用途
+        hasdoublegas:[]//双气
+    },
+    methods:{
+        getHouse:function () {
+            var that = this;
+            var id = this.id;
+            var url = conf.house_info+id;
+            axios.get( url )
+                .then(function (response)
+                {
+                    var data = response.data;
+                    if( data.status == 1 )
+                    {
+                        that.info = data.data;
+                    }
+                })
+        },
+        getData:function () {
+            var url = conf.datas;
+            var that = this;
+            axios.get(url)
+                .then(function (response) {
+                    var data = response.data;
+                    if (data.status == 1) {
+                        //佣金规则
+                        that.commission = data.data[1]['_child'];
+                        //房型
+                        that.roomtype = data.data[2]['_child'];
+                        //装修
+                        that.decoratestyle = data.data[3]['_child'];
+                        //房源标签
+                        that.tagsData = data.data[5]['_child'];
+                    }
+                })
+                .catch(function (error) {
+                    //console.log(error);
+                });
+        },
+        getQueryString:function ( name )
+        {
+            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); // 匹配目标参数
+            var result = window.location.search.substr(1).match(reg); // 对querystring匹配目标参数
+            if (result != null) {
+                return decodeURIComponent(result[2]);
+            }else
+            {
+                return null;
+            }
+        },
+        getDefaultData:function () {
+            var url = conf.datas_defaults;
+            var that = this;
+            axios.get( url )
+                .then(function (response)
+                {
+                    var data = response.data;
+                    if( data.status == 1 )
+                    {
+                        //销售状态
+                        that.salestatus = data.data[7]['_child'];
+                        //朝向
+                        that.orientation = data.data[3]['_child'];
+                        //楼层
+                        that.floorpostion = data.data[2]['_child'];
+                        //权属
+                        that.ownership = data.data[5]['_child'];
+                        //用途
+                        that.purpose = data.data[4]['_child'];
+                        //双气
+                        that.hasdoublegas = data.data[6]['_child'];
+                    }
+                })
+        }
+    },created: function () {
+        var that = this;
+        that.id = that.getQueryString('id');//获取id
+        that.getData();//自定义属性
+        that.getDefaultData();//销售状态
+        that.getHouse();
+    }
+});
--- plugin/js/httpUrl.js
+++ plugin/js/httpUrl.js
@@ -1,9 +1,13 @@
 (function(){
     var host = "http://api.rrzhaofang.com/";
     window.conf={
+        pathUrl:'http://api.rrzhaofang.com/upload/',
         recommend:host+'home/house/recommend',//首页推荐
         datas:host+'home/datas',//前端数据
         datas_default:host+'home/datas-default-one/',//前端自定义属性数据
+        datas_defaults:host+'home/datas-default',//前端自定义属性数据
         house_list:host+'home/house/list',//房源列表
+        house_info:host+'home/house/info/',//房源详情
+        login:host+'home/user/login',//登陆
     }
 })();

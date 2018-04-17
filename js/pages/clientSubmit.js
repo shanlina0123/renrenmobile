43a37372ba691dd1f46a459e5cfd9c9c7484b785
zhaoying111 <2864342582@qq.com>
2018-04-17 19:23:05 +0800

    页面修改

--- js/pages/clientSubmit.js
+++ js/pages/clientSubmit.js
@@ -1,177 +1,154 @@
-// $(function(){
-//
-//     //搜索栏 - 客户状态点击
-//     $(".showText").click(function(){
-//         if($(".selectText").css("display") == "none"){
-//             $(".selectText").show();
-//         }else{
-//             $(".selectText").hide();
-//         }
-//     });
-//
-//
-//     //等级选择效果实现
-//     $(".levelEdit").click(function(){
-//         if($(".levePop").css("display") == "none"){
-//             $(".levePop").show();
-//         }else{
-//             $(".levePop").hide();
-//         }
-//     });
-//     $(".levePop .popUl li").click(function(){
-//         var thisText = $(this).text();
-//         $(".levelShow").text(thisText);
-//         $(".levePop").hide();
-//     });
-//     //客户状态效果实现
-//     $(".stateEdit").click(function(){
-//         if($(".statePop").css("display") == "none"){
-//             $(".statePop").show();
-//         }else{
-//             $(".statePop").hide();
-//         }
-//     });
-//     $(".statePop .popUl li").click(function(){
-//         var thisText = $(this).text();
-//         $(".stateShow").text(thisText);
-//         $(".statePop").hide();
-//     });
-//
-//
-// })
 new Vue({
     el: '#my_vue_client_submit',
     data: {
-        house_name:null,
-        add_params:{"name":null,"mobile":null,"houseid":null,"companyid":null,"remark":null},
-        search_params:{"name":null,"typeid":null,"uuid":null},
-        company_list:[],
-        house_list:[],
-        tokenValue:JSON.parse(sessionStorage.getItem("userinfo")).token
+        house_name: null,
+        add_params: { "name": null, "mobile": null, "houseid": null, "companyid": null, "remark": null },
+        search_params: { "name": null, "typeid": null, "uuid": null },
+        company_list: [],
+        house_list: [],
+        tokenValue: JSON.parse(sessionStorage.getItem("userinfo")).token
     },
-    methods:{
-        GetQueryString:function(name)
-        {
-            var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
-            var url_param=decodeURIComponent(window.location.search)
+    methods: {
+        GetQueryString: function(name) {
+            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+            var url_param = decodeURIComponent(window.location.search)
             var r = url_param.substr(1).match(reg);
-            if(r!=null)return  unescape(r[2]); return null;
+            if (r != null) return unescape(r[2]);
+            return null;
         },
         //其他链接进去的参数获取
-        enterParam:function()
-        {
-            this.search_params.typeid= this.GetQueryString("typeid");
-            this.search_params.uuid=this. GetQueryString("uuid");
+        enterParam: function() {
+            this.search_params.typeid = this.GetQueryString("typeid");
+            this.search_params.uuid = this.GetQueryString("uuid");
         },
         //检查是否内部外部人源
-         checkAfterAdmin:function(){
-             var that = this;
-             if(JSON.parse(that.tokenData).isadminafter==1)
-             {
-                  $(".afteradmin").addClass("isShow");
-             }else{
-                 $(".afteradmin").removeClass("isShow");
-                 that.getCompanyList();//公司列表
-             }
-         },
+        checkAfterAdmin: function() {
+            var that = this;
+            if (JSON.parse(that.tokenData).isadminafter == 1) {
+                $(".afteradmin").addClass("isShow");
+            } else {
+                $(".afteradmin").removeClass("isShow");
+                that.getCompanyList(); //公司列表
+            }
+        },
         //点击显示房源-模糊搜索
-        showHouseClick:function(house_name){
+        showHouseClick: function(house_name) {
             var url = auth_conf.client_houses;
             var that = this;
-            that.house_name=house_name;
-            that.house_name?that.search_params.name=that.house_name:null;
-            axios.post( url,that.search_params,{headers: {"Authorization": that.tokenValue} })
-                .then(function (response)
-                {
+            that.house_name = house_name;
+            that.house_name ? that.search_params.name = that.house_name : null;
+            axios.post(url, that.search_params, { headers: { "Authorization": that.tokenValue } })
+                .then(function(response) {
                     var data = response.data;
-                    if( data.status == 1 )
-                    {
+                    if (data.status == 1) {
                         that.house_list = data.data;
                         $(".forShowSearch").show();
-                    }else if (data.status == 4)
-                    {
+                    } else if (data.status == 4) {
                         that.house_list = data.data;
                         $(".forShowSearch").hide();
-                    }else{
+                    } else {
                         $(".forShowSearch").hide();
                     }
                 })
-                .catch(function (error)
-                {
+                .catch(function(error) {
                     //console.log(error);
                 });
         },
         //点击显示公司
-        showCompanyClick:function(){
-            if($(".formUl")[0].childElementCount==0)
-            {
+        showCompanyClick: function() {
+            if ($(".formUl")[0].childElementCount == 0) {
                 $(".formUl").hide();
-            }else{
+            } else {
                 $(".formUl").show();
             }
 
         },
         //选择房源
-        houseClick:function(id,name){
-            this.add_params.houseid=id;
+        houseClick: function(id, name) {
+            this.add_params.houseid = id;
             $(".selectHouse").val(name);
             $(".forShowSearch").hide();
         },
         //选择公司
-        companyClick:function(id,name){
-            this.add_params.companyid=id;
+        companyClick: function(id, name) {
+            this.add_params.companyid = id;
             $(".showForm").html(name);
             $(".formUl").hide();
         },
         //获取公司列表
-        getCompanyList:function () {
+        getCompanyList: function() {
             var url = auth_conf.client_compnay;
             var that = this;
-            axios.get( url,{headers: {"Authorization":that.tokenValue} })
-                .then(function (response)
-                {
+            axios.get(url, { headers: { "Authorization": that.tokenValue } })
+                .then(function(response) {
                     var data = response.data;
-                    if( data.status == 1 )
-                    {
+                    if (data.status == 1) {
                         that.company_list = data.data;
-                        if(data.data.length==0)
-                        {
+                        if (data.data.length == 0) {
                             $(".formUl").hide();
-                        }else{
+                        } else {
                             $(".formUl").show();
                         }
                     }
                 })
-                .catch(function (error)
-                {
+                .catch(function(error) {
                     //console.log(error);
                 });
         },
         //立即推荐
-        submitClick:function () {
+        submitClick: function() {
             var url = auth_conf.client_refree;
             var that = this;
-            that.add_params.name=that.$refs.name.value;
-            that.add_params.mobile=that.$refs.mobile.value;
-            that.add_params.remark=that.$refs.remark.value;
+            that.validate();
+            that.add_params.name = that.$refs.name.value;
+            that.add_params.mobile = that.$refs.mobile.value;
+            that.add_params.remark = that.$refs.remark.value;
             //token
-            axios.post(url,that.add_params,{headers: {"Authorization": that.tokenValue} })
-                .then(function (response) {
+            axios.post(url, that.add_params, { headers: { "Authorization": that.tokenValue } })
+                .then(function(response) {
                     var data = response.data;
-                    if( data.status == 1 )
-                    {
-                        window.location.href="../pages/myCustomer.html";
+                    if (data.status == 1) {
+                        window.location.href = "../pages/myCustomer.html";
                     }
                     // console.log(response.data.status);
-                }).catch(function (error) {
-                //console.log(error);
-                // console.log(this);
-            });
+                }).catch(function(error) {
+                    //console.log(error);
+                    // console.log(this);
+                });
+        },
+        validate: function() {
+            $(".recommendForm").validate({
+                errorLabelContainer: $(".errorLabel"),
+                rules: {
+                    userName: {
+                        required: true
+                    },
+                    uPhone: {
+                        required: true,
+                        mobile: true
+                    },
+                    selectHouse: {
+                        required: true
+                    }
+                },
+                messages: {
+                    userName: {
+                        required: "请输入客户姓名"
+                    },
+                    uPhone: {
+                        required: "请输入手机号",
+                        mobile: "请正确请输入手机号"
+                    },
+                    selectHouse: {
+                        required: "请选择楼盘"
+                    }
+                }
+            })
         }
-    }
-    ,created: function () {
+    },
+    created: function() {
         var that = this;
-        that.checkAfterAdmin();//检查是否业务员
+        that.checkAfterAdmin(); //检查是否业务员
         that.enterParam();
     }
 });
--- js/pages/newRoomList.js
+++ js/pages/newRoomList.js
@@ -1,65 +1,62 @@
-$(function(){
+$(function() {
     //筛选tab切换
-    $(".tabTable tr .td1").click(function(){
+    $(".tabTable tr .td1").click(function() {
         $(".tabconWrap .areaTab").show().siblings().hide();
     });
-    $(".tabTable tr .td2").click(function(){
+    $(".tabTable tr .td2").click(function() {
         $(".tabconWrap .priceTab").show().siblings().hide();
     });
-    $(".tabTable tr .td3").click(function(){
+    $(".tabTable tr .td3").click(function() {
         $(".tabconWrap .roomTab").show().siblings().hide();
     });
-   /* $(".forTab").click(function(){
-        $(this).hide();
-    })*/
+    //筛选条件选中样式
+    $(".topsort li").click(function() {
+        $(this).addClass("on").siblings().removeClass("on");
+    })
 });
 
 var vm = new Vue({
     el: '#main',
     data: {
-        params:{
-            typeid:1,
-            name:'',
-            price:'',
-            roomtypeid:'',
-            page:1
+        params: {
+            typeid: 1,
+            name: '',
+            price: '',
+            roomtypeid: '',
+            page: 1
         },
-        houseList:[],
-        commission:[],
-        tags:[],
-        roomtype:[],
-        salestatus:[],
-        priceE:'',
-        priceS:'',
-        pages:0
+        houseList: [],
+        commission: [],
+        tags: [],
+        roomtype: [],
+        salestatus: [],
+        priceE: '',
+        priceS: '',
+        pages: 0
     },
-    methods:{
+    methods: {
 
-        getHouseList:function () {
+        getHouseList: function() {
             $(".forTab").hide();
             var url = conf.house_list;
             var that = this;
-            axios.get( url,{params:that.params} )
-                .then(function (response)
-                {
+            axios.get(url, { params: that.params })
+                .then(function(response) {
                     var data = response.data;
-                    if( data.status == 1 )
-                    {
+                    if (data.status == 1) {
                         var list = data.data;
                         that.houseList = list.data;
                         that.pages = list.last_page;
                     }
                 })
         },
-        getData:function () {
+        getData: function() {
             var url = conf.datas;
             var that = this;
-            axios.get( url )
-                .then(function (response)
-                {
+            axios.get(url)
+                .then(function(response) {
                     var data = response.data;
-                    if( data.status == 1 )
-                    {
+                    if (data.status == 1) {
                         //佣金规则
                         that.commission = data.data[1]['_child'];
                         //房型
@@ -69,77 +66,72 @@ var vm = new Vue({
                     }
                 })
         },
-        getDefaultData:function () {
-            var url = conf.datas_default+'7';
+        getDefaultData: function() {
+            var url = conf.datas_default + '7';
             var that = this;
-            axios.get( url )
-                .then(function (response)
-                {
+            axios.get(url)
+                .then(function(response) {
                     var data = response.data;
-                    if( data.status == 1 )
-                    {
+                    if (data.status == 1) {
                         //销售状态
                         that.salestatus = data.data;
                     }
                 })
-                .catch(function (error)
-                {
+                .catch(function(error) {
                     //console.log(error);
                 });
-        },search:function () {
+        },
+        search: function() {
             var name = this.$refs.name.value;
             this.params.name = name;
             this.getHouseList();
-        },//价格区间
-        liData:function ( price ) {
+        }, //价格区间
+        liData: function(price) {
             var that = this;
             that.params.price = price;
             that.priceE = '';
             that.priceS = '';
-        },//房型区间
-        roomData:function ( id ) {
-          this.params.roomtypeid = id;
-        },//开始价格
-        changePriceS:function ( value ) {
+        }, //房型区间
+        roomData: function(id) {
+            this.params.roomtypeid = id;
+        }, //开始价格
+        changePriceS: function(value) {
             this.priceS = value;
-            this.params.price =  value+'-'+this.priceE;
-        },//结束价格
-        changePriceE:function ( value ) {
+            this.params.price = value + '-' + this.priceE;
+        }, //结束价格
+        changePriceE: function(value) {
             this.priceE = value;
-            this.params.price = this.priceS+'-'+value;
-        },//上拉加载
-        handleScroll:function()
-        {
+            this.params.price = this.priceS + '-' + value;
+        }, //上拉加载
+        handleScroll: function() {
             var that = this;
             var lis = that.houseList;
 
         },
-        getQueryString:function ( name )
-        {
+        getQueryString: function(name) {
             var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); // 匹配目标参数
             var result = window.location.search.substr(1).match(reg); // 对querystring匹配目标参数
             if (result != null) {
                 return decodeURIComponent(result[2]);
-            }else
-            {
+            } else {
                 return null;
             }
         },
         //进入推荐页面
-        refreeClick:function (typeid,uuid) {
-            var target_url="typeid="+typeid;
-            if(uuid)
-            {
-                target_url+="&uuid="+uuid;
+        refreeClick: function(typeid, uuid) {
+            var target_url = "typeid=" + typeid;
+            if (uuid) {
+                target_url += "&uuid=" + uuid;
             }
 
-            window.location.href="../pages/recommend.html?"+encodeURIComponent(target_url);
+            window.location.href = "../pages/recommend.html?" + encodeURIComponent(target_url);
         }
-    },created: function () {
+    },
+    created: function() {
         var that = this;
-        that.getDefaultData();//默认属性
-        that.getData();//自定义属性
-        that.params.name = that.getQueryString('name');//获取name
+        that.getDefaultData(); //默认属性
+        that.getData(); //自定义属性
+        that.params.name = that.getQueryString('name'); //获取name
         //that.getHouseList();//新房房源列表
     }
 });
@@ -147,23 +139,22 @@ var vm = new Vue({
 /**
  * 加载分页
  */
-layui.use('flow', function(){
+layui.use('flow', function() {
     var flow = layui.flow;
     flow.load({
         elem: '#roomListUl' //指定列表容器
-        ,done: function(page, next){ //到达临界点（默认滚动触发），触发下一页
+            ,
+        done: function(page, next) { //到达临界点（默认滚动触发），触发下一页
             vm.params.page = page;
             var url = conf.house_list;
             var oldlist = vm.$data.houseList;
-            axios.get( url,{params:vm.$data.params} )
-                .then(function (response)
-                {
+            axios.get(url, { params: vm.$data.params })
+                .then(function(response) {
                     var data = response.data;
-                    if( data.status == 1 )
-                    {
+                    if (data.status == 1) {
                         var list = data.data;
                         vm.$data.pages = list.last_page;
-                        vm.$data.houseList = oldlist.concat( list.data )
+                        vm.$data.houseList = oldlist.concat(list.data)
                     }
                     next($("#roomListUl li").html(), page < vm.$data.pages);
                 })

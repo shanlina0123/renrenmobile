217ba9d6f20769c279b4003bccc345205b67e61f
单莉娜 <1161110682@qq.com>
2018-04-19 13:26:51 +0800

    allow

--- plugin/js/httpUrl.js
+++ plugin/js/httpUrl.js
@@ -1,5 +1,5 @@
 (function () {
-    var host = "http://local.fang.com/";
+    var host = "http://192.168.15.13:8081/";
     //未带toke请求
     window.conf = {
         recommend: host + 'home/house/recommend',//首页推荐
